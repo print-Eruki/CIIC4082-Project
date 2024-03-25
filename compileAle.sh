be64e40c8511df9d11b0e078d43578e67003fd26
print-Eruki <palejorniel@gmail.com>
2024-03-25 12:01:10 -0400

    Created personal bash compilation files

---
+++ compileAle.sh
@@ -0,0 +1,3 @@
+C:\\cc65\\bin\\cl65 background_Project.asm
+C:\\cc65\\bin\\cl65 reset.asm
+C:\\cc65\\bin\\ld65 reset.o background_Project.o -C nes.cfg -o testMovement.nes
---
+++ compileJose.sh
@@ -0,0 +1,3 @@
+ca65 .\background_Project.asm
+ca65 .\reset.asm
+ld65 .\reset.o .\background_Project.o -C .\nes.cfg -o testMovement.nes
