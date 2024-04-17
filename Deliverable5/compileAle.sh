0c0ff42ba946608243c0de7d48b568c52714b8b2
Jose Alejandro <jacorderopr@gmail.com>
2024-04-16 20:52:58 -0400

    initial commit (does not include any collision logic)

---
+++ Deliverable5/compileAle.sh
@@ -0,0 +1,3 @@
+ca65 fifthDeliverable.asm
+ca65 reset.asm
+ld65 reset.o fifthDeliverable.o -C nes.cfg -o fifthDeliverable.nes
---
+++ Deliverable5/maps/background_stage_1.asm
@@ -0,0 +1,33 @@
+background_stage_1_part_1:
+    .byte $aa,$aa,$aa,$aa
+    .byte $8d,$4d,$40,$02
+    .byte $8d,$cd,$fc,$f2
+    .byte $8d,$cd,$cc,$f2
+    .byte $8d,$cd,$cc,$f6
+    .byte $80,$cf,$cc,$f0
+    .byte $b0,$c0,$00,$fe
+    .byte $b0,$cf,$fc,$02
+    .byte $b0,$cc,$00,$02
+    .byte $b0,$ff,$fc,$fe
+    .byte $b0,$43,$0c,$fe
+    .byte $b0,$c3,$0c,$02
+    .byte $00,$c3,$0f,$f2
+    .byte $b0,$c3,$00,$02
+    .byte $aa,$aa,$aa,$aa
+
+background_stage_1_part_2:
+    .byte $aa,$aa,$aa,$aa
+    .byte $95,$40,$00,$32
+    .byte $9f,$ff,$fc,$42
+    .byte $95,$00,$0f,$02
+    .byte $bf,$f0,$5c,$c2
+    .byte $00,$00,$dc,$12
+    .byte $bf,$03,$5f,$1e
+    .byte $bc,$0f,$00,$c6
+    .byte $b0,$04,$0f,$06
+    .byte $80,$0c,$0c,$02
+    .byte $9f,$ff,$0c,$32
+    .byte $8f,$13,$0c,$c0
+    .byte $8f,$33,$ff,$02
+    .byte $80,$30,$00,$02
+    .byte $aa,$aa,$aa,$aa
